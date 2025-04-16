965fa2fe2824a263b79a637b3ace0433e9cfe04b
Sir ThinkTrout <chengxi.li.usa@gmail.com>
2025-04-16 16:34:54 +0000

    added music support, optimized css

--- api/app.py
+++ api/app.py
@@ -1,15 +1,21 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, send_from_directory
 
 # Correct paths from inside /api/
 app = Flask(__name__, static_folder="../static", template_folder="../templates")
 
 @app.route("/")
-def home():
+def menu():
     return render_template("menu.html")
 
 @app.route('/onboarding')
 def onboarding():
     return render_template('onboarding.html')
+
+@app.route('/music/<filename>')
+def music(filename):
+    music_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'music'))
+    return send_from_directory(music_dir, filename)
 
 # This is the magic line — tells Vercel to use Flask as WSGI handler
 # Do NOT wrap it in another function
--- static/js/realinfo.js
+++ static/js/realinfo.js
@@ -2,18 +2,18 @@ document.getElementById('timezone').textContent = Intl.DateTimeFormat().resolved
 document.getElementById('browser-language').textContent = navigator.language.toUpperCase();
 
 function updateTime() {
-const now = new Date();
-const options = { year: 'numeric', month: 'short', day: 'numeric' };
-let formattedDate = now.toLocaleDateString('en-US', options).toUpperCase();
-formattedDate = formattedDate.replace(',', ''); // Remove comma between day and year
-formattedDate = formattedDate.replace(
-formattedDate.split(' ')[1],
-formattedDate.split(' ')[1].slice(0, 3)
-);
-document.getElementById('current-date').textContent = formattedDate;
-document.getElementById('current-time').textContent = now.toLocaleTimeString();
-const daysOfWeek = ['SUNDAY', 'MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY'];
-document.getElementById('day-of-week').textContent = daysOfWeek[now.getDay()];
-}
-setInterval(updateTime, 1000);
+    const now = new Date();
+    const options = { year: 'numeric', month: 'short', day: 'numeric' };
+    let formattedDate = now.toLocaleDateString('en-US', options).toUpperCase();
+    formattedDate = formattedDate.replace(',', ''); // Remove comma between day and year
+    formattedDate = formattedDate.replace(
+    formattedDate.split(' ')[1],
+    formattedDate.split(' ')[1].slice(0, 3)
+    );
+    document.getElementById('current-date').textContent = formattedDate;
+    document.getElementById('current-time').textContent = now.toLocaleTimeString();
+    const daysOfWeek = ['SUNDAY', 'MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY'];
+    document.getElementById('day-of-week').textContent = daysOfWeek[now.getDay()];
+    }
+    setInterval(updateTime, 1000);
 updateTime();
